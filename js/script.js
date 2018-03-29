351ba0bbc40d3f01e477eff6a3d20a9ed08570d5
SHAYNA BAUER <2016100493@UC.ACAD>
2018-03-29 10:44:12 -0300

    adicionando e removendo da cesta

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 var cesta_frutas = [];
 
 function adicionar() {
-    var item = document.getElementById("cesta").value;
+    //var item = document.getElementById("item").value;
+    var item = $("#item").val();
     cesta_frutas.push(item);
-    alert(cesta_frutas);
-}
+    //alert(cesta_frutas);
+    //var cesta = document.getElementById("cesta");
+    //cesta.innerHTML = cesta_frutas;
+    if(item === "x") {
+        $("#cesta").slideUp(900);
+    } else {
+        $("#cesta").append("<p id='idItem'>"+ item + "</p>").slideDown(900);
+    }}
+
+    function remover() {
+        var item = $("#item").val();
+        cesta_frutas.pop(item);
+        console.log(cesta_frutas);               
+    }
